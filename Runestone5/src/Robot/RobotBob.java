8ed86658504fb8d0927548fe749a034338d3edd5
Simon Ljus <simonljus@Simons-MacBook-Pro.local>
2017-04-27 15:57:38 +0200

    folder

---
+++ Runestone5/src/Robot/RobotBob.java
@@ -0,0 +1,28 @@
+package Robot;
+
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.port.SensorPort;
+import lejos.utility.Delay;
+
+public class RobotBob {
+
+	public static void main(String[] args) {
+		LCD.drawString("Plugin Test", 0, 4);
+		//Delay.msDelay(5000);
+		//Motor.B.rotateTo( 360 *4);
+		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
+		SampleProvider sampleProvider = colorSensor.getColorIDMode();
+		float[] samples = new float[sampleProvider.sampleSize()];
+        while(true){
+        	sampleProvider.fetchSample(samples, 0);
+        	LCD.drawString(samples[0] + "", 0, 2);
+        }
+		
+		
+	}
+
+}
