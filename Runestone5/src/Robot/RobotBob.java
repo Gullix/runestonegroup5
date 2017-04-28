5418d943a0315ceeb029a82b051dba8463084eee
Simon Ljus <simon.ljus94@gmail.com>
2017-04-28 13:51:47 +0200

    robot bletooth working

--- Runestone5/src/Robot/RobotBob.java
+++ Runestone5/src/Robot/RobotBob.java
@@ -1,6 +1,8 @@
 package Robot;
 
 import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 import lejos.hardware.Bluetooth;
 import lejos.hardware.LocalBTDevice;
@@ -18,7 +20,8 @@ public class RobotBob {
 
 	public static void main(String[] args) {
 		LCD.drawString("Plugin Test", 0, 4);
-		String PyServer= "74:DF:BF:4A:17:62";
+		String PyServer= "00:0C:78:76:64:DB";
+		
 		//Delay.msDelay(5000);
 		//Motor.B.rotateTo( 360 *4);
 		//hello
@@ -32,41 +35,63 @@ public class RobotBob {
         	LCD.drawString(samples[0] + "", 0, 2);
         }
         */
-		LocalBTDevice btDevice = Bluetooth.getLocalDevice();
-		btDevice.setVisibility(true);
-		//LCD.drawString("removing", 0, 2);
-		Delay.msDelay(3000);
-		LCD.clearDisplay();
-		//LCD.drawString("connecting", 0, 2);
-		//btDevice.authenticate("74:DF:BF:4A:17:62", "");
-		String name = "noadress";
-		name = btDevice.getBluetoothAddress();
-		LCD.clearDisplay();
-		LCD.drawString(name, 0, 2);
-		Delay.msDelay(2000);
-		LCD.drawString("Test Done", 0, 4);
-		Delay.msDelay(2000);
+		//LocalBTDevice btDevice = Bluetooth.getLocalDevice();
+		//btDevice.setVisibility(true);
+		//String name = "noadress";
+		//name = btDevice.getBluetoothAddress();
+		
 		NXTCommConnector nxtCom = Bluetooth.getNXTCommConnector();
 		LCD.clearDisplay();
     	LCD.drawString("connecting", 0, 2);
-    	Delay.msDelay(3000);
-    	
-		NXTConnection mConnection= nxtCom.connect(PyServer,3);
-    	DataOutputStream dos = mConnection.openDataOutputStream();
+    	NXTConnection mConnection= nxtCom.connect(PyServer,2);
 		LCD.clearDisplay();
     	LCD.drawString("trying", 0, 2);
-		Delay.msDelay(3000);
       
 		  try{
-			  dos.writeBytes("Hello server");
+			String message = "hello server from Bob";
+			byte[] bMessage = message.getBytes(StandardCharsets.UTF_8);
+			  mConnection.write(bMessage,bMessage.length);
+			  byte[] sMessage = new byte[1024];
+			  mConnection.read(sMessage, 1024);
+			  LCD.clearDisplay();
+			  
+			  String str = new String(sMessage, StandardCharsets.UTF_8);
+			  LCD.drawString(str + "", 0, 2);
+			  Delay.msDelay(3000);
+			 
 	        }
 	        catch(Exception e){
 	        	LCD.clearDisplay();
 	        	LCD.drawString("exception", 0, 2);
 	    		Delay.msDelay(3000);
 	        }
-	        
-		
-		
-	}
+		  boolean talkWithServer = true;
+		  try{
+          while(talkWithServer){
+        	  byte[] sMessage = new byte[1024];
+			  mConnection.read(sMessage, 1024);
+			  String str = new String(sMessage, StandardCharsets.UTF_8);
+			  if (str.equals("DONE")){
+				 talkWithServer = false;
+				 mConnection.close();
+				 break;
+			  }
+			  else{
+				  switch(str){
+				  case("FORWARD"):
+					  
+					  break;
+				  case("LEFT"):
+					  
+				  }
+			  }
+          }
+		  }
+          
+          
+          catch(Exception e){
+        	  
+          }
+	}  
+          
 }
