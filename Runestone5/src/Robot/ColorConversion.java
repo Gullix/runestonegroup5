e2cdd45fda7ba1c64d02f9692cec2fb902067c27
Robert Gulliksson <Robert Gulliksson>
2017-05-04 22:15:58 +0200

    colorconversion done

--- Runestone5/src/Robot/ColorConversion.java
+++ Runestone5/src/Robot/ColorConversion.java
@@ -38,25 +38,61 @@ public class ColorConversion {
 		float refY = (float) 100.000;
 		float refZ = (float) 98.723;
 		float var_X = xyz[1] / refX;
-		float var_Y = xyz[1] / refX;
-		float var_Z = xyz[1] / refX;
+		float var_Y = xyz[2] / refX;
+		float var_Z = xyz[3] / refX;
+		if ( var_X > 0.008856 ) 
+			var_X = (float) Math.pow(var_X,  ( 1/3 ));
+		else                    
+			var_X = (float) (( 7.787 * var_X ) + ( 16 / 116 ));
 		
+		if ( var_Y > 0.008856 ) 
+			var_Y = (float) Math.pow(var_Y,  ( 1/3 ));
+		else                    
+			var_Y = (float) (( 7.787 * var_Y ) + ( 16 / 116 ));
+		
+		if ( var_Z > 0.008856 ) 
+			var_Z = (float) Math.pow(var_Z,  ( 1/3 ));
+		else                    
+			var_Z = (float) (( 7.787 * var_Z ) + ( 16 / 116 ));
+		
+		
+		float l = (116 * var_Y) - 16;
+		float a = 500 * ( var_X - var_Y);
+		float b = 200 * ( var_Y - var_Z);
 		float[] lab = {l,a,b};
 		return lab;
 	}
 	
 	public float[] RgbToLab(float[] rgb)
-	{}
+	{
+		return XyzToLab(rbgToXYZ(rgb));
+		
+	}
 	
 	public float Euclidean(float[] color1, float[] color2)
 	{
-		
+		float[] lab1 = RgbToLab(color1);
+		float[] lab2 = RgbToLab(color2);
+		float deltaE = (float) Math.sqrt(Math.pow(lab1[1],lab2[1])+Math.pow(lab1[2],lab2[2])+Math.pow(lab1[3],lab2[3]));
+		return deltaE;
 	}
 	//Loop through each float array in colors and compare eeuclidean with value. Return index of most similar.
 	public int MostSimilar(float[][] colors, float[] value)
 	{
-		
-		
+		int minInd = 0;
+		float minVal = Euclidean(colors[0],value);
+		float auxE = 0;
+		for (int i = 0; i < colors.length; i++) {
+				auxE = Euclidean(colors[i],value);
+		      	if(auxE < minVal)
+		      	{
+		      		minInd = i;
+		      		minVal = auxE;
+		      	}
+		      
+		    
+		}
+		return minInd;
 	}
 	
 }
