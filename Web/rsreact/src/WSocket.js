07d9777bef16763b0c9340b30d9d52b3e29864ab
Simon Ljus <simon.ljus94@gmail.com>
2017-05-15 15:13:02 +0200

    GUI can now receivbe messages from server, server can not send correctly though

--- Web/rsreact/src/WSocket.js
+++ Web/rsreact/src/WSocket.js
@@ -2,24 +2,37 @@
  * Created by simonljus on 2017-05-09.
  */
 var mWSocket;
-class WSocket{
+import React, { Component } from 'react';
+class WSocket extends Component{
 
-    constructor(){
+    constructor(props){
+        super();
+        this.state=({
+          message: ""
+        });
         var loc = location.hostname;
+        var that = this;
         //var loc = "130.238.94.57"
-        let serverAddress = "ws://" + loc + ":" + 8001;
+        var serverAddress = "ws://" + loc + ":" + 8001;
+        console.log(serverAddress);
         mWSocket = new WebSocket(serverAddress);
         mWSocket.onopen = function(event){
 
         };
-        mWSocket.onclose = function(event){
+        mWSocket.onclose= function(event){
 
         }
+        mWSocket.onmessage = function(evt) {
+            that.messageFromServer(evt.data);
+        };
     }
     sendToServer(data){
         console.log(data)
         mWSocket.send(data);
 
+    }
+    messageFromServer(msg){
+        console.log("FROM SERVER: " + msg);
     }
 }
 
