86513199da198a76e565843b85e6836fae38823a
Maxwell Clarke <maxeonyx@gmail.com>
2017-05-22 11:46:54 +0200

    map in shared data

--- Server/shared_data.py
+++ Server/shared_data.py
@@ -7,23 +7,22 @@ def new_shared_dict():
             "last_target": None,
             "direction": "F",
             "position": [0,0] #row column (think matrix)
+        },
+        "map": {
+	        "rows" : [
+                ["b","b","z","b","z","b","z","b","b"],
+                ["b","b","z","b","z","b","z","b","b"],
+                ["s","l","i","l","i","l","i","l","e"],
+                ["b","b","l","b","l","b","l","b","b"],
+                ["b","b","z","b","z","b","z","b","b"]
+            ],
+            "square_types": {
+                "b": "blank"
+                "z": "zone"
+                "s": "start"
+                "e": "end"
+                "l": "line",
+                "i": "intersection"
             }
-        "map":{
-           "rows" : [
-            {["","","z","","z","","z","",""],
-             ["","","z","","z","","z","",""]
-
-            }
-           ]
-
-         "square_types"{
-         "": "blank"
-         "z":  "zone"
-         "s":   "start"
-         "e":  "end"
-         "l":  "line"
-
-         }
         }
-
-     }
+    }
