533c0391d759d619b93d5ff39ed5ed14168a0e7e
Gullix <rob_5_337@hotmail.com>
2017-05-23 15:52:05 +0200

    here is more arm code courtesy of Oliver Stein

--- Runestone5/src/Robot/Move.java
+++ Runestone5/src/Robot/Move.java
@@ -1,5 +1,8 @@
 package Robot;
 
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.robotics.navigation.MovePilot;
 
 public class Move {
@@ -7,13 +10,15 @@ public class Move {
 	private String direction;
 	private String cm;
 	private MovePilot mp;
+	private EV3MediumRegulatedMotor arm = new EV3MediumRegulatedMotor(MotorPort.D);
 	
 	public Move(String d, String c, MovePilot m){
 		direction = d;
 		cm = c;
 		mp = m;
+		arm.rotate(90);
+
 	}
-	
 	public String getDirection(){ return this.direction; }
 	public String getCm(){ return this.cm;}
 	public MovePilot getMp(){ return this.mp;}
