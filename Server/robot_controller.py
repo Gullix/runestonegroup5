eddb4eab1fbcced21fc0ece68d0a5dc7cb4c24a0
Gullix <rob_5_337@hotmail.com>
2017-05-23 11:25:38 +0200

    calculate direction etc

--- Server/robot_controller.py
+++ Server/robot_controller.py
@@ -1,7 +1,37 @@
-def get_target(data, robot_sock):
+def get_target(data):
 
     dir = data["robot"]["direction"]
-    command = input("Give me something\n")
-    #target = "M," + dir + ",10"
-    #robot_sock.send(bytes(target, "utf-8"))
-    robot_sock.send(bytes(command, "utf-8"))
+    robpos = data["robot"]["position"]
+    data["robot"]["target"] = data["targets"].popleft()
+    
+def update_position(data, direction):
+	if direction == "U":
+		data["robot"]["position"][] =
+	if direction == "D":
+
+	if direction == "R":
+
+	if direction == "L":
+		
+
+
+
+def calculate_dir(data)
+	tarR, tarC = data["robot"]["target"]
+	posR, posC = data["robot"]["position"]
+	if (posR == tarR) && (tarC == tarC):
+		return "S"
+	if posR == 2:
+		if posC == tarC:
+			if posR < tarR:
+				return "U"
+			if posR > tarR:
+				return "D"
+		if posC < tarC:
+			return "R"
+		if posC > tarC:
+			return "L"
+	if posR < 2:
+		return "D"
+	if posR > 2:
+		return "U"
