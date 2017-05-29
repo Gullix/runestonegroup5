054d7999630d6be464445764693610709a87a6c3
Simon Ljus <simon.ljus94@gmail.com>
2017-05-29 12:56:06 +0200

    gui

--- Web/rsreact/src/Manual/RobotController.js
+++ Web/rsreact/src/Manual/RobotController.js
@@ -7,23 +7,41 @@ require('./RobotController.css');
 import PropTypes from 'prop-types';
 // For controlling the robot manually
 class RobotController extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            manualMode: false
+        }
+    }
 
-    //
-    robotMoveToServer(rdir, rdistance){
-        var message = {
-            type_of_data: "manual",
-            data: rdir
+    robotMoveToServer(manual_command){
+
+        let message = {
+            type_of_data: "manual_command",
+            data: manual_command
         };
 
         this.props.wsSend(message);
     }
-    connectionSwitch(status){
-        document.getElementById("wsConnectionButton");
-
+    toggleManual(){
+       var  manual_mode = this.state.manualMode
+         var message = {
+             type_of_data: "manual_on",
+             data: null
+         }
+         if(manual_mode){
+            message = {
+                 type_of_data: "manual_off",
+                 data: null
+             };
+         }
+         this.setState({
+             manualMode: !manual_mode
+        });
+        this.props.wsSend(message);
     }
-
     victoryToServer() {
-        var message = {
+        let message = {
             type_of_data: "victory",
             data: null
         };
@@ -33,17 +51,31 @@ class RobotController extends Component{
 
 
     render(){
+        var styleObj={
+            color: "white",
+            verticalAlign: "middle"
+        }
         return(
             <div>
+                <div>
                 <div id="remoteContainer">
                     <div id="topButtons">
-                        <button id="forward_button"   className="moveButton btn btn-primary" onClick={this.robotMoveToServer.bind(this,"F","10")}> ^ </button>
+                        <button id="forward_button"   className="moveButton btn btn-primary" onClick={this.robotMoveToServer.bind(this,"F")}> ^ </button>
                     </div>
                     <div id="bottom_buttons">
-                        <button id="left_button"      className="moveButton btn btn-primary" onClick={this.robotMoveToServer.bind(this,"L","10")}> &lt; </button>
-                        <button id="backward_button"  className="moveButton btn btn-primary" onClick={this.robotMoveToServer.bind(this,"B","10")}> v </button>
-                        <button id="right_button"     className="moveButton btn btn-primary" onClick={this.robotMoveToServer.bind(this,"R","10")}> &gt; </button>
+                        <button id="left_button"      className="moveButton btn btn-primary" onClick={this.robotMoveToServer.bind(this,"L")}> &lt; </button>
+                        <button id="backward_button"  className="moveButton btn btn-primary" onClick={this.robotMoveToServer.bind(this,"B")}> v </button>
+                        <button id="right_button"     className="moveButton btn btn-primary" onClick={this.robotMoveToServer.bind(this,"R")}> &gt; </button>
                     </div>
+                </div>
+                <button id="stop_button"      className="moveButton btn btn-primary" onClick={this.robotMoveToServer.bind(this,"X")}>STOP</button>
+                </div>
+                <div>
+                <span style={styleObj}>Manual toggle</span>
+                <label className="switch" onChange={this.toggleManual.bind(this)}>
+                    <input type="checkbox" checked={this.state.manualMode}/>
+                    <div className="slider" />
+                </label>
                 </div>
                 <div>
                     <button id="victory_button" onClick={this.victoryToServer.bind(this)}>VICTORY</button>
